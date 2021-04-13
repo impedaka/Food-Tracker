8eb227d2baddd90f667973b228663b1c61415e10
Alice Yu <75595719+impedaka@users.noreply.github.com>
2021-04-13 13:54:49 -0400

    Update foods-list.js

--- src/components/foods-list.js
+++ src/components/foods-list.js
@@ -1,72 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
-const Food = props => (
-  <tr>
-    <td>{props.food.username}</td>
-    <td>{props.food.description}</td>
-    <td>{props.food.calories}</td>
-    <td>{props.food.date.substring(0,10)}</td>
-    <td>
-      <Link to={"/edit/"+props.food._id}>edit</Link> | <a href="#" onClick={() => { props.deleteFood(props.food._id) }}>delete</a>
-    </td>
-  </tr>
-)
+const FoodsList = () => {
+  const [Data, setData] = useState([]);
 
-export default class FoodsList extends Component {
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    axios.get("http://localhost:3000/foods/").then((response) => {
+      response.data.map((data) => {
+        setData((oldArray) => [...oldArray, data]);
+        return 0;
+      });
+    });
+  }, []);
 
-    this.deleteFood = this.deleteFood.bind(this)
+  const deleteExercise = (id) => {
+    axios.delete("http://localhost:3000/foods/" + id).then((response) => {
+      console.log(response.data);
+    });
 
-    this.state = {foods: []};
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:3000/foods/')
-      .then(response => {
-        this.setState({ foods: response.data })
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
-
-  deleteFood(id) {
-    axios.delete('http://localhost:3000/foods/'+id)
-      .then(response => { console.log(response.data)});
-
-    this.setState({
-      foods: this.state.foods.filter(el => el._id !== id)
-    })
-  }
-
-  foodList() {
-    return this.state.foods.map(currentfood => {
-      return <Food food={currentfood} deleteFood={this.deleteFood} key={currentfood._id}/>;
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Logged Foods</h3>
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Username</th>
-              <th>Description</th>
-              <th>Calories</th>
-              <th>Date</th>
-              <th>Foods</th>
+    setData(Data.filter((el) => { return el._id !== id }))
+  };
+  return (
+    <div>
+      <h3>Logged Foods</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>Username</th>
+            <th>Description</th>
+            <th>Calories</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Data.map((data, index) => (
+            <tr key={index}>
+              <td>{data.username}</td>
+              <td>{data.description}</td>
+              <td>{data.calories}</td>
+              <td>{data.date.substring(0, 10)}</td>
+              <td>
+                <Link className="btn btn-primary" to={"/edit/" + data._id}>Edit</Link> 
+                <button className="btn btn-danger"
+                  href='/'
+                  onClick={() => {
+                    deleteExercise(data._id);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            { this.foodList() }
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-}
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default FoodsList;
