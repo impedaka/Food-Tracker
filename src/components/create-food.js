a2cc136df9f150ac62d90de389712936d3e25cbf
Alice Yu <75595719+impedaka@users.noreply.github.com>
2021-04-13 13:52:20 -0400

    Update create-food.js

--- src/components/create-food.js
+++ src/components/create-food.js
@@ -1,221 +1,108 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import DatePicker from 'react-datepicker';
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
-export default class CreateFood extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.onChangeUsername = this.onChangeUsername.bind(this);
-      this.onChangeDescription = this.onChangeDescription.bind(this);
-      this.onChangecalories = this.onChangecalories.bind(this);
-      this.onChangeDate = this.onChangeDate.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
-  
-      this.state = {
-        username: '',
-        description: '',
-        calories: 0,
-        date: new Date(),
-        users: []
-      }
-    }
-  
-    componentDidMount() {
-      axios.get('http://localhost:3000/users/')
-        .then(response => {
-          if (response.data.length > 0) {
-            this.setState({
-              users: response.data.map(user => user.username),
-              username: response.data[0].username
-            })
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-  
-    }
-  
-    onChangeUsername(e) {
-      this.setState({
-        username: e.target.value
-      })
-    }
-  
-    onChangeDescription(e) {
-      this.setState({
-        description: e.target.value
-      })
-    }
-  
-    onChangecalories(e) {
-      this.setState({
-        calories: e.target.value
-      })
-    }
-  
-    onChangeDate(date) {
-      this.setState({
-        date: date
-      })
-    }
-  
-    onSubmit(e) {
-      e.preventDefault();
-  
-      const food = {
-        username: this.state.username,
-        description: this.state.description,
-        calories: this.state.calories,
-        date: this.state.date
-      }
-  
-      console.log(food);
-  
-      axios.post('http://localhost:3000/foods/add', food)
-        .then(res => console.log(res.data));
-  
+const CreateFood = () => {
+  const [Food, setFood] = useState({
+    username: "hello",
+    description: "",
+    calories: 0,
+    date: new Date(),
+  });
+
+  const [Users, setUsers] = useState([])
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/foods/add", Food)
+      .then(res => console.log(res.data));
       window.location = '/';
-    }
-  
-    render() {
-      return (
-      <div>
-        <h3>Create New Food Log</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group"> 
-            <label>Username: </label>
-            <select ref="userInput"
-                required
-                className="form-control"
-                value={this.state.username}
-                onChange={this.onChangeUsername}>
-                {
-                  this.state.users.map(function(user) {
-                    return <option 
-                      key={user}
-                      value={user}>{user}
-                      </option>;
-                  })
-                }
-            </select>
-          </div>
-          <div className="form-group"> 
-            <label>Description: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.description}
-                onChange={this.onChangeDescription}
-                />
-          </div>
-          <div className="form-group">
-            <label>Calories: </label>
-            <input 
-                type="text" 
-                className="form-control"
-                value={this.state.calories}
-                onChange={this.onChangecalories}
-                />
-          </div>
-          <div className="form-group">
-            <label>Date: </label>
-            <div>
-              <DatePicker
-                selected={this.state.date}
-                onChange={this.onChangeDate}
-              />
-            </div>
-          </div>
-  
-          <div className="form-group">
-            <input type="submit" value="Create Food Log" className="btn btn-primary" />
-          </div>
-        </form>
-      </div>
-      )
-    }
-  }
+  };
 
-{/* const CreateFood = () => {
-    const [postData, setPostData] = useState({ username: '', description: '', calories: 0, date: new Date(), users: []})
-
-    const handleSubmit = () => {
-
-    }
-    return (
-        <div>
-          <h3>Create New Food Log</h3>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group"> 
-              <label>Username: </label>
-              <select ref="userInput"
-                  required
-                  className="form-control"
-                  value={postData.username}
-                  onChange={(e) =>  setPostData({ ...postData, username: e.target.value})}>
-                  {
-                    users.map(function(user) {
-                      return <option 
-                        key={user}
-                        value={user}>{user}
-                        </option>;
-                    })
-                  }
-              </select>
-            </div>
-            <div className="form-group"> 
-              <label>Description: </label>
-              <input  type="text"
-                  required
-                  className="form-control"
-                  value={description}
-                  onChange={(e) =>  setPostData({ ...postData, description: e.target.value})}
-                  />
-            </div>
-            <div className="form-group">
-              <label>Calories: </label>
-              <input 
-                  type="text" 
-                  className="form-control"
-                  value={calories}
-                  onChange={(e) =>  setPostData({ ...postData, calories: e.target.value})}
-                  />
-            </div>
-            <div className="form-group">
-              <label>Date: </label>
-              <div>
-                <DatePicker
-                  selected={date}
-                  onChange={(e) =>  setPostData({ ...postData, date: e.target.value})}
-                />
-              </div>
-            </div>
-    
-            <div className="form-group">
-              <input type="submit" value="Create Food Log" className="btn btn-primary" />
-            </div>
-          </form>
-        </div>
-        )
-      }
-
-useEffect(() => {
-    axios.get('http://localhost:3000/users/')
-    .then(response => {
-      if (response.data.length > 0) {
-        setPostData({
-          users: response.data.map(user => user.username),
-          username: response.data[0].username
+  useEffect( () => {
+     axios
+      .get("http://localhost:3000/users")
+      .then(res => {
+        res.data.map(user => {
+          setUsers(oldArray => [...oldArray, user.username])
+          return 0;
         })
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+      })
 
+      
   }, []);
 
-export default CreateFood; */}
+
+  return (
+    <div>
+      <h3>Create New Food Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            required
+            className="form-control"
+            value={Food.username}
+            onChange={(e) =>
+              setFood({ ...Food, username: e.target.value })
+            }
+          >
+            {Users.map(function (user) {
+              return (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={Food.description}
+            onChange={(e) =>
+              setFood({ ...Food, description: e.target.value })
+            }
+          />
+        </div>
+        <div className="form-group">
+          <label>Calories: </label>
+          <input
+            type="text"
+            className="form-control"
+            value={Food.calories}
+            onChange={(e) =>
+              setFood({ ...Food, calories: e.target.value })
+            }
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={Food.date}
+              onChange={(e) =>
+                setFood({ ...Food, date: e.target.value })
+              }
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Food Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateFood;
